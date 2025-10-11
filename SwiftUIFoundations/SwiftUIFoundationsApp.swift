a7ef58ab16ba221cebbd91dc63714a7008c975d1
Dion James Smith <dion.smith070@gmail.com>
2025-10-12 00:31:17 +0530

    Initial Commit

---
+++ SwiftUIFoundations/SwiftUIFoundationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFoundationsApp.swift
+//  SwiftUIFoundations
+//
+//  Created by Dion James Smith on 12/10/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFoundationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
