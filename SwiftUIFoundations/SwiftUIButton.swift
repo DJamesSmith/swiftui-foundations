459ce954e49b1167409a7d69320075e182bb0400
Dion James Smith <dion.smith@live.com>
2025-10-16 16:22:33 +0530

    swiftUI buttons

---
+++ SwiftUIFoundations/SwiftUIButton.swift
@@ -0,0 +1,34 @@
+//
+//  SwiftUIButton.swift
+//  SwiftUIFoundations
+//
+//  Created by Dion James Smith on 16/10/25.
+//
+
+import SwiftUI
+
+struct SwiftUIButton: View {
+    var title: String
+    var textColor: Color
+    var backgroundColor: Color
+
+    var body: some View {
+        VStack(spacing: 20) {
+            Text(title)
+                .bold()
+                .frame(width: 280, height: 50)
+                .background(backgroundColor.gradient)
+                .cornerRadius(10)
+            
+            Text(title)
+                .frame(width: 280, height: 50)
+                .background(backgroundColor.gradient)
+                .cornerRadius(10)
+        }
+        .foregroundColor(textColor)
+//        .font(.system(size: 20, weight: .bold, design: .default))
+    }
+}
+#Preview {
+    SwiftUIButton(title: "SwiftUI Button", textColor: .white, backgroundColor: .blue)
+}
